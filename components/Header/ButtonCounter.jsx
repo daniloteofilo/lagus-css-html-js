b040755b0b16d538a47d3887c4877755d5000061
Danilo Teofilo <danigolaseu@gmail.com>
2023-06-11 23:38:04 -0300

    Correção da função do botão de clique

--- components/Header/ButtonCounter.jsx
+++ components/Header/ButtonCounter.jsx
@@ -3,13 +3,13 @@ import { ButtonStyled } from "./styles";
 
 
 export default function ButtonCounter() {
-    let [currentValue, setCurrentValue] = useState(1);
-    const icrementValue = () => {
-        setCurrentValue(currentValue++)
+    const  [currentValue, setCurrentValue] = useState(1);
+    const incrementValue = () => {
+        setCurrentValue(currentValue+1)
     }
 
     return (
-        <ButtonStyled onClick={()=> icrementValue()}>
+        <ButtonStyled onClick={incrementValue}>
             clicked {currentValue} {currentValue > 1 ? `times`: `time`}
         </ButtonStyled>
     );
